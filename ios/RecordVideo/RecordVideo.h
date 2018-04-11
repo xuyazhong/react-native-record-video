b82157f1b571f0352c5948fd02b92525759749f7
xuyazhong <xuyazhong@foxmail.com>
2018-04-11 15:46:22 +0800

    init

---
+++ ios/RecordVideo/RecordVideo.h
@@ -0,0 +1,13 @@
+//
+//  RecordVideo.h
+//  RecordVideo
+//
+//  Created by xuyazhong on 2018/4/11.
+//  Copyright © 2018年 xuyazhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RecordVideo : NSObject
+
+@end
---
+++ ios/RecordVideo/RecordVideo.m
@@ -0,0 +1,13 @@
+//
+//  RecordVideo.m
+//  RecordVideo
+//
+//  Created by xuyazhong on 2018/4/11.
+//  Copyright © 2018年 xuyazhong. All rights reserved.
+//
+
+#import "RecordVideo.h"
+
+@implementation RecordVideo
+
+@end
